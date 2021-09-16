5329dec9571bbe434292514bad5b00629005f565
Talen Fisher <talen.fisher@cythral.com>
2021-09-16 14:26:13 -0500

    Fix Watch Command

--- watch.sh
+++ watch.sh
@@ -2,13 +2,21 @@
 
 inode_to_watch=$1
 command_to_run=$2
+lastRunTime=$(date +'%H%M%S')
 
-$command_to_run &
+eval "$command_to_run &"
 pid=$!
 
 inotifywait -q -m -e modify $inode_to_watch |
 while read -r filename event; do
-    while kill -15 $pid 2>/dev/null; do sleep 1; done;
-    $command_to_run &
-    pid=$!
+    currentTime=$(date +'%H%M%S')
+    delta=$(expr $currentTime - $lastRunTime)
+
+    if [ "$delta" != "0" ]; then
+        kill -15 $pid 2>/dev/null;
+        while ps | awk '{ print $1 }' | grep -w "$pid" > /dev/null 2>&1; do sleep 1; done
+        eval "$command_to_run &"
+        pid=$!
+        lastRunTime=$(date +'%H%M%S')
+    fi
 done
