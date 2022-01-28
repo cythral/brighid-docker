f2ed26b019679b7a2b80238f25dd5751b2cc4515
Talen Fisher <talen.fisher@cythral.com>
2022-01-28 15:07:09 -0600

    Migrations script hotfix

--- src/migrate.sh
+++ src/migrate.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 wget $MIGRATIONS_BUNDLE_URL -O ./run-migrations
 chmod +x ./run-migrations
 
-Database__Password=$(decrs $ENCRYPTED_DATABASE_PASSWORD) || exit 1
+export Database__Password=$(decrs $Encrypted__Database__Password) || exit 1
 ./run-migrations
